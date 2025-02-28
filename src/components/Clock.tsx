5683169100c97de42d70bfc97acffdb7f387eab4
Marius Grigaliunas <cia.marius.grigaliunas@gmail.com>
2025-02-28 12:32:02 +0100

    added Increment and Decrement of the time intervals

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,9 +1,29 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 export default function () {
 
     const [breakLength, setBreakLength] = useState<number>(5)
     const [sessionLength, setSessionLength] = useState<number>(25)
+
+
+    const Decrement = (intervalName: string) => {
+        if(intervalName === "break") {
+            setBreakLength(breakLength - 1)
+        } else if (intervalName === "session") {
+            setSessionLength(sessionLength - 1)
+        }
+
+    }
+
+    const Increment = (intervalName: string) => {
+        if(intervalName === "break") {
+            setBreakLength(breakLength + 1)
+        } else if (intervalName === "session") {
+            setSessionLength(sessionLength + 1)
+        }
+
+    }
+
 
     return (
         <div className='flex flex-col h-lvh w-lvw justify-center items-center'>
@@ -12,17 +32,17 @@ export default function () {
                 <fieldset id='break-field'>
                     <label id='break-label'>Break Length</label>
                     <div id='break-controls' className='flex gap-2'>
-                        <button id='break-decrement'>DOWN</button>
+                        <button id='break-decrement' onClick={() => (Decrement("break"))} >DOWN</button>
                         <output id='break-length' className='text-2xl'>{breakLength}</output>
-                        <button id='break-increment'>UP</button>
+                        <button id='break-increment' onClick={() => (Increment("break"))} >UP</button>
                     </div>
                 </fieldset>
                 <fieldset id='session-field'>
                     <label id='break-label'>Session Length</label>
                     <div id='session-controls' className='flex gap-2'>
-                        <button id='session-decrement'>DOWN</button>
+                        <button id='session-decrement' onClick={() => (Decrement("session"))} >DOWN</button>
                         <output id='session-length' className='text-2xl'>{sessionLength}</output>
-                        <button id='session-increment'>UP</button>
+                        <button id='session-increment' onClick={() => (Increment("session"))} >UP</button>
                     </div>
                 </fieldset>
             </section>
