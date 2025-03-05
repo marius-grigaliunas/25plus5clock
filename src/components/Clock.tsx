058fe1b269234b7255111437a43597dd3aa3fcda
Marius Grigaliunas <cia.marius.grigaliunas@gmail.com>
2025-03-05 20:50:16 +0100

    reworked the logic, simplified code

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,66 +1,50 @@
 import { useEffect, useRef, useState } from 'react'
 
+type IntervalType = "break" | "session"
+
 export default function () {
 
     const [breakLengthInMinutes, setbreakLengthInMinutes] = useState<number>(5)
-    const [breakLenghtInSeconds, setBreakLengthInSeconds] = useState<number>(breakLengthInMinutes * 60)
-
     const [sessionLengthInMinutes, setSessionLengthInMinutes] = useState<number>(25)
-    const [sessionLengthInSeconds, setSessionLengthInSeconds] = useState<number>(sessionLengthInMinutes * 60)
-    
+
+    const [currentTime, setCurrentTime] = useState<number>(sessionLengthInMinutes * 60)
+
     const [isSession, setIsSession] = useState<boolean>(true)
     const [isPaused, setIsPaused] = useState<boolean>(true)
 
     const audioRef = useRef<HTMLAudioElement>(null)
 
 
-    const Reset = () => {
+    const reset = () => {
         setIsPaused(true)
         setIsSession(true)
         setbreakLengthInMinutes(5)
         setSessionLengthInMinutes(25)
-        setBreakLengthInSeconds(5 * 60)
-        setSessionLengthInSeconds(25 * 60)
-        stopSound()
+        setCurrentTime(25*60)
+        stopSound()  
     }
 
-    const Decrement = (intervalName: string) => {
-        if(intervalName === "break") {
-            if(breakLengthInMinutes > 1) {
-                setbreakLengthInMinutes(breakLengthInMinutes - 1)
-                if(isPaused) {
-                    setBreakLengthInSeconds((breakLengthInMinutes - 1) * 60)
-                }
-            }
-        } else if (intervalName === "session") {
-            if(sessionLengthInMinutes > 1) {
-                setSessionLengthInMinutes(sessionLengthInMinutes - 1)
-                if(isPaused) {
-                    setSessionLengthInSeconds((sessionLengthInMinutes - 1) * 60)
-                }
-            }
+    const adjustInterval = (intervalType: IntervalType, amount: number) => {
+        const isBreak = intervalType === "break"
+        const currentLength = isBreak ? breakLengthInMinutes : sessionLengthInMinutes
+        const setLength = isBreak ? setbreakLengthInMinutes : setSessionLengthInMinutes
+
+        if(currentLength + amount >= 1 && currentLength + amount <= 60) {
+            setLength(currentLength + amount)
+
+            if(isPaused && isBreak && !isSession)
+                adjustCurrentTime(amount * 60)
+            else if (isPaused && !isBreak && isSession)
+                adjustCurrentTime(amount*60)
+
+            //isPaused && adjustCurrentTime(amount * 60)
         }
-
     }
 
-    const Increment = (intervalName: string) => {
-        if(intervalName === "break") {
-            if(breakLengthInMinutes < 60) {
-                setbreakLengthInMinutes(breakLengthInMinutes + 1)
-                if(isPaused) {
-                    setBreakLengthInSeconds((breakLengthInMinutes + 1) * 60)
-                }
-            }
-        } else if (intervalName === "session") {
-            if(sessionLengthInMinutes < 60) {
-                setSessionLengthInMinutes(sessionLengthInMinutes + 1)
-                if(isPaused) {
-                    setSessionLengthInSeconds((sessionLengthInMinutes + 1) * 60)
-                }
-            }
-        }
-
+    const adjustCurrentTime = (amount: number) => {
+        setCurrentTime((prevTime: number) => prevTime + amount)
     }
+
 
     const formatTime = (timeInSeconds: number): string => {
         const minutes = Math.floor(timeInSeconds / 60)
@@ -74,43 +58,32 @@ export default function () {
 
         if(!isPaused) {
             intervalID = setInterval(() => {
-                if(isSession) {
-                    setSessionLengthInSeconds((prevTime: number) => {
-                        if(prevTime <= 0) {
-                            clearInterval(intervalID)
+                setCurrentTime((prevTime: number) => {
+                    if(prevTime <= 0) {
+                        clearInterval(intervalID)
+                        playSound()
+
+                        if(isSession) {
                             setIsSession(false)
-                            console.log(`new session value ${sessionLengthInMinutes * 60}`)
-                            playSound()
+                            return (breakLengthInMinutes * 60)
+                        } else {
+                            setIsSession(true)
                             return (sessionLengthInMinutes * 60)
                         }
-                        return prevTime - 1
-                    })
-                } else {
-                    setBreakLengthInSeconds((prevTime: number) => {
-                        if(prevTime <= 0) {
-                            clearInterval(intervalID)
-                            setIsSession(true)
-                            console.log(`new break value ${breakLengthInMinutes * 60}`)
-                            playSound()
-                            return (breakLengthInMinutes * 60)
-                        }
-                        return prevTime - 1
-                    })
-                }
+
+                    }
+                    return prevTime - 1
+                })
             }, 1000)
         }
 
         return () => {
             clearInterval(intervalID)
         }
-    }, [isPaused, isSession, sessionLengthInSeconds, breakLenghtInSeconds] )
+    }, [isPaused, isSession, breakLengthInMinutes, sessionLengthInMinutes] )
 
-    const StartPause = () => {
-        if(isPaused) {
-            setIsPaused(false)
-        } else {
-            setIsPaused(true)
-        }
+    const startPause = () => {
+        setIsPaused(prevState => !prevState)
     }
 
     const playSound = () => {
@@ -136,35 +109,35 @@ export default function () {
                 <fieldset id='break-field'>
                     <label id='break-label'>Break Length</label>
                     <div id='break-controls' className='flex gap-2'>
-                        <button id='break-decrement' onClick={() => (Decrement("break"))} >DOWN</button>
+                        <button id='break-decrement' onClick={() => (adjustInterval("break", -1))} >DOWN</button>
                         <output id="break-length" className='text-2xl'>{breakLengthInMinutes}</output>
-                        <button id='break-increment' onClick={() => (Increment("break"))} >UP</button>
+                        <button id='break-increment' onClick={() => (adjustInterval("break", +1))} >UP</button>
                     </div>
                 </fieldset>
                 <fieldset id='session-field'>
                     <label id='session-label'>Session Length</label>
                     <div id='session-controls' className='flex gap-2'>
-                        <button id='session-decrement' onClick={() => (Decrement("session"))} >DOWN</button>
+                        <button id='session-decrement' onClick={() => (adjustInterval("session", -1))} >DOWN</button>
                         <output id='session-length' className='text-2xl'>{sessionLengthInMinutes}</output>
-                        <button id='session-increment' onClick={() => (Increment("session"))} >UP</button>
+                        <button id='session-increment' onClick={() => (adjustInterval("session", +1))} >UP</button>
                     </div>
                 </fieldset>
             </section>
             <section id="timer-controls-section" className='my-10 text-2xl flex gap-7'>
-                <button id='start_stop' onClick={StartPause}>{isPaused ? "START" : "STOP"}</button>
-                <button id='reset' onClick={Reset}>RESET</button>
+                <button id='start_stop' onClick={startPause}>{isPaused ? "START" : "STOP"}</button>
+                <button id='reset' onClick={reset}>RESET</button>
             </section>
             <section id='timer-section' className='my-5 border border-black w-100 h-52 flex justify-evenly items-center flex-col'>
                 <h2 id='timer-label' className='text-3xl'>{isSession ? "Session" : "Break"}</h2>
                 <h2 id='time-left' className='text-7xl'>
-                    {isSession ? `${formatTime(sessionLengthInSeconds)}`:`${(formatTime(breakLenghtInSeconds))}`}
+                    {formatTime(currentTime)}
                 </h2>
                 <audio 
                     id='beep'
                     ref={audioRef}
                 >
                     <source src='/public/beep.mp3' type='audio/mp3'/>
-                    Your browser does not support the audio element.
+                    When the timer ends, you are supposed to hear a beep, but your browser does not support the audio element.
                 </audio>
             </section>
         </div>
