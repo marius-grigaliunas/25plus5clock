1f897e18363ec0b27a5da2dcd9db815f6fc0609c
Marius Grigaliunas <cia.marius.grigaliunas@gmail.com>
2025-03-08 00:13:24 +0100

    Implementing IntervalOption, going to sleep

--- src/components/clock/IntervalOption.tsx
+++ src/components/clock/IntervalOption.tsx
@@ -1,15 +1,40 @@
 
-export const IntervalOption = () => {
-  return (
-        <section className='flex flex-row justify-center items-center gap-10'>
-            <fieldset id='break-field'>
-                <label id='break-label'>Break Length</label>
-                <div id='break-controls' className='flex gap-2'>
-                    <button id='break-decrement' onClick={() => (adjustInterval("break", -1))} >DOWN</button>
-                    <output id="break-length" className='text-2xl'>{breakLengthInMinutes}</output>
-                    <button id='break-increment' onClick={() => (adjustInterval("break", +1))} >UP</button>
-                </div>
-            </fieldset>
-        </section>
+interface IntervalOptionProps {
+    intervalType: string
+    breakLength: number
+
+}
+
+export const IntervalOption = ({intervalType, breakLength,} : IntervalOptionProps) => {
+  
+    const adjustInterval = (intervalType: IntervalType, amount: number) => {
+        const isBreak = intervalType === "break"
+        const currentLength = isBreak ? breakLengthInMinutes : sessionLengthInMinutes
+        const setLength = isBreak ? setbreakLengthInMinutes : setSessionLengthInMinutes
+
+        if(currentLength + amount >= 1 && currentLength + amount <= 60) {
+            setLength(currentLength + amount)
+
+            if(isPaused && isBreak && !isSession)
+                adjustCurrentTime(amount * 60)
+            else if (isPaused && !isBreak && isSession)
+                adjustCurrentTime(amount*60)
+
+        }
+    }
+
+    const adjustCurrentTime = (amount: number) => {
+        setCurrentTime((prevTime: number) => prevTime + amount)
+    }
+  
+    return (
+        <fieldset id={`${intervalType}-field`} >
+            <label id={`${intervalType}-label`}>{intervalType} Length</label>
+            <div id={`${intervalType}-controls`} className='flex gap-2'>
+                <button id={`${intervalType}-decrement`} onClick={() => (adjustInterval("break", -1))} >DOWN</button>
+                <output id={`${intervalType}-length`} className='text-2xl'>{breakLength}</output>
+                <button id={`${intervalType}-increment`} onClick={() => (adjustInterval("break", +1))} >UP</button>
+            </div>
+        </fieldset>
     )
 }
